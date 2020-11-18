0a1af6c68b31cd249812a91acc73fe08051f281c
casey van schaarenburg <533029@student.glu.nl>
2020-11-18 11:06:05 +0100

    volledig werkende coronaproof qr code source code

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
-<?php
-if (!defined('DBHOST')) define("DBHOST","localhost");
-if (!defined('DBNAME')) define("DBNAME","webshop");
-if (!defined('DBUSER')) define("DBUSER","root");
-if (!defined('DBPASS')) define("DBPASS","");
 
 
-$con = new mysqli(DBHOST,DBUSER,DBPASS,DBNAME);
-if(!$con or mysqli_connect_error()) {
-    echo mysqli_error($con);
-    # code...
-}else {
-    $con->set_charset("utf8");
-    $con->query("SET lc_tine_names = 'nl_NL';");
-}
-?>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -35,6 +19,8 @@ if(!$con or mysqli_connect_error()) {
         body {
             font-family: "Open Sans", sans-serif;
         }
+
+        }
         h2 {
             color: #000;
             font-size: 26px;
@@ -43,100 +29,43 @@ if(!$con or mysqli_connect_error()) {
             text-transform: uppercase;
             position: relative;
             margin: 30px 0 80px;
+
         }
         h2 b {
             color: hotpink;
         }
-        h2::after {
-            content: "";
-            width: 100px;
-            position: absolute;
-            margin: 0 auto;
-            height: 4px;
-            background: rgba(0, 0, 0, 0.2);
-            left: 0;
-            right: 0;
-            bottom: -20px;
-        }
-        .carousel {
-            margin: 50px auto;
-            padding: 0 70px;
-        }
-        .carousel .item {
-            min-height: 330px;
-            text-align: center;
-            overflow: hidden;
-        }
-        .carousel .item .img-box {
-            height: 160px;
-            width: 100%;
+        #slideshow {
+            margin: 80px auto;
             position: relative;
+            width: 1000px;
+            height: 240px;
+            padding: 10px;
+
         }
-        .carousel .item img {
-            max-width: 100%;
-            max-height: 100%;
-            display: inline-block;
+
+        #slideshow > div {
             position: absolute;
-            bottom: 0;
-            margin: 0 auto;
-            left: 0;
-            right: 0;
+            top: 10px;
+            left: 10px;
+            right: 10px;
+            bottom: 10px;
         }
-        .carousel .item h4 {
-            font-size: 18px;
-            margin: 10px 0;
+        .img-box img{
+            max-height: 200px;
         }
-        .carousel .item .btn {
-            color: #333;
-            border-radius: 0;
-            font-size: 11px;
-            text-transform: uppercase;
-            font-weight: bold;
-            background: none;
-            border: 1px solid #ccc;
-            padding: 5px 10px;
-            margin-top: 5px;
-            line-height: 16px;
+        .img-box2 img{
+            max-height: 200px;
+            padding: 0;
         }
-        .carousel .item .btn:hover, .carousel .item .btn:focus {
-            color: #fff;
-            background: #000;
-            border-color: #000;
-            box-shadow: none;
-        }
-        .carousel .item .btn i {
-            font-size: 14px;
-            font-weight: bold;
-            margin-left: 5px;
-        }
-        .carousel .thumb-wrapper {
+        .thumb-wrapper {
             text-align: center;
+            border-bottom: inset;
         }
-        .carousel .thumb-content {
+        .thumb-content {
             padding: 15px;
         }
-        .carousel .carousel-control {
-            height: 100px;
-            width: 40px;
-            background: none;
-            margin: auto 0;
-            background: rgba(0, 0, 0, 0.2);
-        }
-        .carousel .carousel-control i {
-            font-size: 30px;
-            position: absolute;
-            top: 50%;
-            display: inline-block;
-            margin: -16px 0 0 0;
-            z-index: 5;
-            left: 0;
-            right: 0;
-            color: rgba(0, 0, 0, 0.8);
-            text-shadow: none;
-            font-weight: bold;
-        }
-        .carousel .item-price {
-            font-size: 13px;
+        .item-price {
+            font-size: 18px;
             padding: 2px 0;
         }
         .carousel .item-price strike {
@@ -145,36 +74,17 @@ if(!$con or mysqli_connect_error()) {
         }
         .carousel .item-price span {
             color: #86bd57;
-            font-size: 110%;
-        }
-        .carousel .carousel-control.left i {
-            margin-left: -3px;
-        }
-        .carousel .carousel-control.left i {
-            margin-right: -3px;
-        }
-        .carousel .carousel-indicators {
-            bottom: -50px;
-        }
-        .carousel-indicators li, .carousel-indicators li.active {
-            width: 10px;
-            height: 10px;
-            margin: 4px;
-            border-radius: 50%;
-            border-color: transparent;
-        }
-        .carousel-indicators li {
-            background: rgba(0, 0, 0, 0.2);
-        }
-        .carousel-indicators li.active {
-            background: rgba(0, 0, 0, 0.6);
+            font-size: 100%;
         }
         .star-rating li {
             padding: 0;
         }
         .star-rating i {
-            font-size: 14px;
+            font-size: 17px;
             color: #ffc000;
+        }
+        .naam h4 {
+            font-size: 30px;
         }
     </style>
 </head>
@@ -182,38 +92,45 @@ if(!$con or mysqli_connect_error()) {
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h2>Trending <b>Products</b></h2>
+           <div class="logo">
+            <img src="img/hotpink.png" alt="" style="width: 300px; margin-left: 410px; margin-bottom: 25px; margin-top: 25px" >
+           </div>
+            <hr>
+            <h2 style="text-align: center">Trending <b>Products</b></h2>
 
-            <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
-                <!-- Carousel indicators -->
-                <!-- Wrapper for carousel items -->
-                <?php
-                    $response = file_get_contents('http://api-hotpink.test/product/read_all.php');
-                    $data = json_decode($response, true);
+            <?php
+            $response = file_get_contents('http://u533029.gluweb.nl/hotpink-api/product/read_all.php');
+            $data = json_decode($response, true);
 
-                    // print_r($data);
+            $response4 = file_get_contents('http://u533029.gluweb.nl/hotpink-api/product/read2.php');
+            $data4 = json_decode($response4, true);
+            // print_r($data);
 
-                ?>
-                <div class="carousel-inner">
-                    <div class="item carousel-item active">
-                        <div class="row">
+            ?>
+
+
+            <div id="slideshow">
+                <div>
+                    <div class="row">
                         <?php
                         foreach ($data as $item){
-                            $response2 = file_get_contents('http://api-hotpink.test/product/image.php?productid='.$item['id']);
+                            $response2 = file_get_contents('http://u533029.gluweb.nl/hotpink-api/product/image.php?productid='.$item['id']);
                             $data2 = json_decode($response2, true);
-//                            var_dump($data2);
 
-
+                            $response3 = file_get_contents('http://u533029.gluweb.nl/hotpink-api/product/qr.php?id='.$item['id']);
+                            $data3 = json_decode($response3, true);
                             ?>
-                            <div class="col-sm-3">
+                            <div class="col-sm-3" style="height:430px; z-index: -1;">
                                 <div class="thumb-wrapper">
                                     <div class="img-box">
                                         <?php
-                                         echo '<img src="img/'.$data2['image'].'" class="img-fluid">';
+                                        echo '<img src="img/'.$data2['image'].'" class="img-fluid" style="height 100px">';
                                         ?>
                                     </div>
                                     <div class="thumb-content">
-                                        <h4><?php echo $item["name"]; ?></h4>
+
+                                        <h3 class="naam"><?php echo $item["name"]; ?></h3>
+                                        <hr>
                                         <p class="item-price"><strike>$400.00</strike> <span>$ <?php echo $item["price"]; ?></span></p>
                                         <div class="star-rating">
                                             <ul class="list-inline">
@@ -224,27 +141,79 @@ if(!$con or mysqli_connect_error()) {
                                                 <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
                                             </ul>
                                         </div>
+                                        <div class="img-box2">
+                                            <?php
+                                            echo '<img src="img/'.$data3['image'].'" class="img-fluid" style="height:120px; margin-top:10px">';
+                                            ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         <?php }?>
-                        </div>
                     </div>
                 </div>
-                <img src="img/qr-code.png" style="float: left; width: 170px; margin-left: 20px ">
-                <img src="img/qr-code2.png" style="float: left; width: 170px; margin-left: 85px ">
-                <img src="img/qr-code3.png" style="float: left; width: 170px; margin-left: 85px ">
-                <img src="img/qr-code4.png" style="float: left; width: 170px; margin-left: 85px ">
-                <!-- Carousel controls -->
-                <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
-                    <i class="fa fa-angle-left"></i>
-                </a>
-                <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
-                    <i class="fa fa-angle-right"></i>
-                </a>
+                <div>
+                    <div class="row">
+                        <?php
+                        foreach ($data4 as $item){
+                            $response2 = file_get_contents('http://u533029.gluweb.nl/hotpink-api/product/image.php?productid='.$item['id']);
+                            $data2 = json_decode($response2, true);
+
+                            $response3 = file_get_contents('http://u533029.gluweb.nl/hotpink-api/product/qr.php?id='.$item['id']);
+                            $data3 = json_decode($response3, true);
+                            ?>
+                            <div class="col-sm-3" style="height:430px; z-index: -1;">
+                                <div class="thumb-wrapper">
+                                    <div class="img-box">
+                                        <?php
+                                        echo '<img src="img/'.$data2['image'].'" class="img-fluid">';
+                                        ?>
+                                    </div>
+                                    <div class="thumb-content">
+
+                                        <h3 class="naam"><?php echo $item["name"]; ?></h3>
+                                        <hr>
+                                        <p class="item-price"><strike>$400.00</strike> <span>$ <?php echo $item["price"]; ?></span></p>
+                                        <div class="star-rating">
+                                            <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                            </ul>
+                                        </div>
+                                        <div class="img-box2">
+                                            <?php
+                                            echo '<img src="img/'.$data3['image'].'" class="img-fluid" style="height:120px; margin-top:10px">';
+                                            ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php }?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
 </body>
+<script>
+    $("#slideshow > div:gt(0)").hide();
+
+    setInterval(function() {
+        $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    }, 4000);
+</script>
+<link href="//maxcdn.sbootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 </html>
